f85683a794cf225e157549a332a6e565c35486fa
PanghoChao <87929190+PanghoChao@users.noreply.github.com>
2021-10-07 18:21:57 +0900

    Update vowel_dictionary.py
    https://programmers.co.kr/learn/courses/30/lessons/84512

--- step2/vowel_dictionary.py
+++ step2/vowel_dictionary.py
@@ -1,3 +1,19 @@
+#등비수열 문제 
+#그리고 각 자리당 공식은 
+'''
+마지막 자리 =  +1 (A = 1, E= 2, I=3 ,O=4, U=5)
+
+네번째 자리 = (마지막자리 x 5)  + 1
+
+세번째 자리 = (네번째자리 x 5)  + 1
+
+두번째 자리 = (세번째자리 x 5)  + 1
+
+첫번째 자리 = (두번째자리 x 5)  + 1
+
+첫번째 자리 공식  
+'''
+
 ##규칙을 찾아 별도 배열을 만들어 계산##
 
 
@@ -11,4 +27,24 @@ def solution(word):
     return answer
   
   
-  # 재귀적 표현으로 계산
+### 재귀적 표현으로 계산 ###
+def rowel_dict(n:int)->int:
+    if n == 1  :
+        return 1
+    n -= 1
+    squence_num = rowel_dict(n) * 5 + 1
+    return squence_num
+    
+def solution(word):
+    words = ["A","E","I","O","U"]
+    answer = 0
+    for idx, alph in enumerate(word):
+        answer += rowel_dict(5-idx) * words.index(alph) + 1
+    
+        
+    return answer
+
+
+
+#문제 사이트 
+# https://programmers.co.kr/learn/courses/30/lessons/84512
