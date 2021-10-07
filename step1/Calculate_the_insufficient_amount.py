99ab18f748e1622eac61e6b865778b24bde245ea
PanghoChao <87929190+PanghoChao@users.noreply.github.com>
2021-10-07 15:57:53 +0900

    Create Calculate_the_insufficient_amount.py
    https://programmers.co.kr/learn/courses/30/lessons/82612

---
+++ step1/Calculate_the_insufficient_amount.py
@@ -0,0 +1,30 @@
+##### first code ####
+
+def solution(price, money, count):
+    answer = 0
+    for i in range(1, count +1 ):
+        answer += (price * i)
+    
+
+    return (answer - money)
+
+
+
+
+
+#### repair code ####
+
+def solution(price, money, count):
+    all_price = 0
+    for i in range(1, count +1 ):
+        all_price += (price * i)
+    
+    if all_price < money:
+        answer = 0
+    else :
+        answer = all_price -money
+
+    return answer
+    
+    
+    
