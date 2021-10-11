9c350cb9d840bf3ab6a2269a4104eb174c57c695
PanghoChao <87929190+PanghoChao@users.noreply.github.com>
2021-10-11 16:04:45 +0900

    Update open_ChatingRoom.py

--- step2/open_ChatingRoom.py
+++ step2/open_ChatingRoom.py
@@ -21,3 +21,39 @@ def solution(record):
 
     
     return answer
+
+#but too late, too mach
+#I want to reduce the time.
+
+#I recycled "record fuction"
+
+
+
+def solution(record):
+    uid_dict ={}
+    act_list = []
+    answer = []
+    for infor in record:
+        p_infor = infor.split()
+        if p_infor[0] != 'Leave':
+            uid_dict[p_infor[1]] = p_infor[2]
+            
+        
+    
+    for infor2 in record:
+        p_infor2 =infor2.split()
+        out_put, uid = p_infor2[0] , p_infor2[1]
+        
+        if out_put == 'Enter' :
+            answer.append(uid_dict[uid]+"님이 들어왔습니다.")
+        if out_put == 'Leave' :
+            answer.append(uid_dict[uid]+"님이 나갔습니다.")
+        else: #change일때
+            continue
+
+    
+    return answer
+
+
+#result: runtime is almost half time
+#very satisfy
