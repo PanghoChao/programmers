e4d59bb5e20121e376ec45b9662a3d65a0648eaa
PanghoChao <87929190+PanghoChao@users.noreply.github.com>
2021-10-12 22:23:37 +0900

    Update k_number.py

--- step1/k_number.py
+++ step1/k_number.py
@@ -9,7 +9,15 @@ def solution(array, commands):
     answer = []
     for i, j, k in commands :
         cut = array[i-1:j]
+        if i > j :
+            continue
+            #indexerror
         if i != j:
             cut.sort()
+        if j-i-1 < k :
+            continue
+            #indexerror
         answer.append(cut[k-1])
     return answer
+
+# except 는 편의상 continue 로 대체
