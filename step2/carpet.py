7c87a1f5f5ac21f06bd0ffb3f4633a14413c8a42
PanghoChao <87929190+PanghoChao@users.noreply.github.com>
2021-10-31 23:12:37 +0900

    Create carpet.py

---
+++ step2/carpet.py
@@ -0,0 +1,22 @@
+# 문제이해 
+
+
+# code
+def solution(brown, yellow):
+ 
+    divis = []
+    grsr = yellow + brown
+    for i in range(1, int(grsr**(1/2)) +1): #약수 범위
+        if grsr % i == 0 : 
+            a = grsr// i
+            divis.append([a,i]) 
+            
+            
+    for garo, sero in divis :
+        if (brown == (garo+ sero -2) *2) and (yellow == (garo - 2)* (sero - 2)): #답이면
+            answer= [garo,sero]
+    return answer
+  
+  
+  #success 
+  #score(+1)
